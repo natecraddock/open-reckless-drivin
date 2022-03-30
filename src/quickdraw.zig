2a7a4987cc1412f17e7e4e1c37bbda9498a23297
Nathan Craddock <nzcraddock@gmail.com>
2022-03-29 21:16:01 -0600

    feat: add run-length decoding of PackBits data

--- src/quickdraw.zig
+++ src/quickdraw.zig
@@ -5,6 +5,8 @@
 
 const std = @import("std");
 
+const Allocator = std.mem.Allocator;
+
 /// TODO: This will likely be pulled out to also be used for reading values
 /// from sprites and other resources.
 const Reader = struct {
@@ -50,11 +52,13 @@ const Reader = struct {
 /// Both versions have a header before the image data, but for Reckless Drivin'
 /// the only useful information from the headers (after parsing) is the size of
 /// the image. Everything else can be ignored.
-const QuickDraw = struct {
+pub const QuickDraw = struct {
     width: u16,
     height: u16,
+    pixels: []Pixel = undefined,
 
-    fn parse(bytes: []const u8) !QuickDraw {
+    /// Parse a QuickDraw image from a given sequence of bytes
+    pub fn parse(allocator: Allocator, bytes: []const u8) !QuickDraw {
         var reader = Reader.init(bytes);
 
         // Parse header
@@ -71,6 +75,9 @@ const QuickDraw = struct {
             try reader.skip(22);
         }
 
+        // TODO: allocate a buffer to store pixel data
+        var image: QuickDraw = .{ .width = width, .height = height };
+
         // Parse remaining opcodes to construct the bitmap data
         while (reader.readNumber(u16)) |op| {
             switch (op) {
@@ -81,11 +88,11 @@ const QuickDraw = struct {
                 },
                 0x0098 => {
                     // PackBitsRect
-                    // TODO: get allocated pixels (or error) here
+                    // image.pixels = try parsePackBitsRect(&reader);
                 },
                 0x009A => {
                     // DirectBitsRect
-                    // TODO: get allocated pixels (or error) here
+                    image.pixels = try parseDirectBitsRect(allocator, &reader);
                 },
                 0x00A1 => {
                     // LongComment (ignored)
@@ -100,7 +107,123 @@ const QuickDraw = struct {
             // handle EOF when image not yet constructed
         }
 
-        return QuickDraw{ .width = width, .height = height };
+        return image;
+    }
+};
+
+pub const Pixel = struct { r: u8, g: u8, b: u8 };
+
+fn parseDirectBitsRect(allocator: Allocator, reader: *Reader) ![]Pixel {
+    const map = try PixMap.parse(reader);
+    try reader.skip(18); // srcRect, dstRect, mode
+
+    // const is_indexed = map.pixel_type == 0;
+    // const is_packed = map.pixel_size == 8;
+    const row_bytes = map.row_bytes & 0b0011_1111_1111_1111;
+
+    const width = @intCast(u32, map.right - map.left);
+    const num_pixels = @intCast(u32, map.bottom - map.top) * @intCast(u32, map.right - map.left);
+    var pixels = try allocator.alloc(Pixel, num_pixels);
+
+    // Now read the actual pixel data
+    const height = map.bottom - map.top;
+    var line: usize = 0;
+    while (line < height) : (line += 1) {
+        var byte_count: usize = if (row_bytes > 250) try reader.readNumber(u16) else try reader.readNumber(u8);
+        _ = byte_count;
+        try unpackRow(reader, &map, pixels[line * width .. line * width + width]);
+    }
+
+    return pixels;
+}
+
+/// Run-length decode a row of PackBits.
+///
+/// Although it seems like DirectBitsRect would be uncompressed, and PackBitsRect
+/// would be compressed, PackBitsRect actually refers to indexing in a color table.
+/// Both types of images can store linewise run-length encoded bitmap data. The
+/// data is byte-oriented rather than pixel oriented. This information is missing
+/// from Imaging with Quickdraw. See http://fileformats.archiveteam.org/wiki/PackBits
+/// for more details.
+fn unpackRow(reader: *Reader, map: *const PixMap, pixels: []Pixel) !void {
+    var col: usize = 0;
+    while (col < pixels.len) {
+        var n = try reader.readNumber(u8);
+        if (n <= 127) {
+            // Interpret the next n+1 bytes literally
+            const repeat = n + 1;
+            if (map.pixel_size == 8) {} else if (map.pixel_size == 16) {
+                var i: usize = 0;
+                while (i < repeat) : ({
+                    i += 1;
+                    col += 1;
+                }) {
+                    const data = try reader.readNumber(u16);
+                    pixels[col].r = @intCast(u8, (data & 0b0111_1100_0000_0000) >> 10);
+                    pixels[col].g = @intCast(u8, (data & 0b0000_0011_1110_0000) >> 5);
+                    pixels[col].b = @intCast(u8, data & 0b0000_0000_0001_1111);
+                }
+            }
+        } else if (n == 128) {
+            // Ignore
+        } else {
+            // Repeat the next byte 257 - n times
+            const repeat = 257 - @intCast(u9, n);
+            if (map.pixel_size == 8) {} else if (map.pixel_size == 16) {
+                const data = try reader.readNumber(u16);
+                const pixel: Pixel = .{
+                    .r = @intCast(u8, (data & 0b0111_1100_0000_0000) >> 10),
+                    .g = @intCast(u8, (data & 0b0000_0011_1110_0000) >> 5),
+                    .b = @intCast(u8, data & 0b0000_0000_0001_1111),
+                };
+                var i: usize = 0;
+                while (i < repeat) : ({
+                    i += 1;
+                    col += 1;
+                }) {
+                    pixels[col] = pixel;
+                }
+            }
+        }
+    }
+}
+
+/// A pixel map contains information about the dimensions and contents
+/// of a pixel image. See 4-46 in Imaging With Quickdraw for reference.
+/// Reckless Drivin' only needs a subset of the full data.
+const PixMap = struct {
+    row_bytes: u16,
+    pack_type: u16,
+    pixel_type: u16,
+    pixel_size: u16,
+    top: u16,
+    left: u16,
+    bottom: u16,
+    right: u16,
+
+    fn parse(reader: *Reader) !PixMap {
+        try reader.skip(4); // baseAddr
+        const row_bytes = try reader.readNumber(u16);
+        const top = try reader.readNumber(u16);
+        const left = try reader.readNumber(u16);
+        const bottom = try reader.readNumber(u16);
+        const right = try reader.readNumber(u16);
+        try reader.skip(2); // pmVersion
+        const pack_type = try reader.readNumber(u16);
+        try reader.skip(12); // packSize, hRes, and vRes
+        const pixel_type = try reader.readNumber(u16);
+        const pixel_size = try reader.readNumber(u16);
+        try reader.skip(16); // cmpCount, cmpSize, planeBytes, and pmTable
+        return PixMap{
+            .row_bytes = row_bytes,
+            .pack_type = pack_type,
+            .pixel_type = pixel_type,
+            .pixel_size = pixel_size,
+            .top = top,
+            .left = left,
+            .bottom = bottom,
+            .right = right,
+        };
     }
 };
 
@@ -112,9 +235,10 @@ test "quickdraw parsing" {
 
     const resource = resources.getResource("PPic", 1000) orelse return;
     const decompressed = try lzrw.decompressResource(testing.allocator, resource);
+    defer testing.allocator.free(decompressed);
 
-    const qd = QuickDraw.parse(decompressed);
-    std.debug.print("{}\n", .{qd});
+    const qd = try QuickDraw.parse(testing.allocator, decompressed);
+    defer testing.allocator.free(qd.pixels);
 
     // try expect(getResource("PPic", 1001) != null);
     // try expect(getResource("PPic", 1002) != null);
